5427dd1bc7705b6081ab37c78ccb1f251dc35b65
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2021-11-14 14:32:40 +0300

    Check tasks 1-5

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ console.log(answer);
 const myAnswer = document.getElementsByTagName('li')[1].textContent;
 answer.insertAdjacentHTML('beforeend', myAnswer);//вставляет в html
 
+// Гораздо проще вставлять текст в узлы-элементы через запись в свойства textContent или innerText
+
 
 // передаем ответ в константу и устанавливаем мдля элемента с классом answer атрибут с именем selected-option и значением порядковым номером выбранного варианта ответа.
 answer.setAttribute('selected-option', 2);
+
+// Защищённые data атрибуты можно также установить через динамическое создание нового свойства у объекта dataset у объекта узла-элемента – точно в таком же стиле, как у обычных объектов
 
 
 //скрыть вариант ответа
@@ -32,3 +36,6 @@ list.insertAdjacentHTML('beforeend', newChoice);
 console.log(list);
 console.log(newChoice);
 
+
+// А где же решения заданий по установке стилей через классы и методы объекта classList и через объект style?
+// Присылайте остальные решения следующим коммитом в этой ветке и сообщите об этом в телеграмм. Пока что оценку выставлять не буду.
